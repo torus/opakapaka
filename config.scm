a88525376442622c357e9a852130d7c96488a4b6
Toru Hisai <toru-github@torus.jp>
2009-12-28 20:34:51 -1000

    added config file.

---
+++ config.scm
@@ -0,0 +1,14 @@
+(define-module config-module
+  (define (title t) (cons 'title t))
+  )
+
+(define-macro opakapaka-config
+  (lambda procs
+    `(define *config-alist*
+       (let1 mod (find-module 'config-module)
+         (list
+          ,@(map (lambda (p)
+                   `(eval ',p mod)) procs))))))
+
+(define (config-get key)
+  (cdr (assoc key *config-alist*)))
---
+++ opakapaka.conf.cgi
@@ -0,0 +1,5 @@
+;; -*- scheme -*-
+
+(opakapaka-config
+ (title "Opakapaka Chat")
+ )
